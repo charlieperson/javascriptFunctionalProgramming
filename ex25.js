0d19905bfacca8a36f3d981cd2a1233bdbe782f4
charlieperson <charlie.person@hyperisland.se>
2016-07-01 11:27:10 -0500

    Completes exercise 25

--- ex25.js
+++ ex25.js
@@ -32,5 +32,17 @@ function() {
 			}
 		];
 
-	return lists. // complete this expression
-};
+    return lists.map(function(list) {
+		return {
+			name: list.name,
+			videos:
+				videos.
+					filter(function(video) {
+						return video.listId === list.id;
+					}).
+					map(function(video) {
+						return {id: video.id, title: video.title};
+					})
+		};
+	});
+}

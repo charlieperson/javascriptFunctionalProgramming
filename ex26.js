1f82910857f0a2f2dce05bfd5c1bc4ec5fa9b1df
charlieperson <charlie.person@hyperisland.se>
2016-07-03 21:29:37 -0500

    Completes exercise 26

--- ex26.js
+++ ex26.js
@@ -49,6 +49,30 @@ function() {
 			{ videoId: 654356453, time: 984934 }
 		];
 
-	return lists. // complete this expression
+		return lists.map(function(list) {
+		return {
+			name: list.name,
+			videos:
+				videos.
+					filter(function(video) {
+						return video.listId === list.id;
+					}).
+					concatMap(function(video) {
+						return Array.zip(
+							bookmarks.filter(function(bookmark) {
+								return bookmark.videoId === video.id;
+							}),
+							boxarts.filter(function(boxart) {
+								return boxart.videoId === video.id;
+							}).
+							reduce(function(acc,curr) {
+								return acc.width * acc.height < curr.width * curr.height ? acc : curr;
+							}),
+							function(bookmark, boxart) {
+								return { id: video.id, title: video.title, time: bookmark.time, boxart: boxart.url };
+							});
+				})
+		};
+	});
 
 }

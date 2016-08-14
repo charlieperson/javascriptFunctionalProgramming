74f922fd57001f53dcee95b4a0164f262e70d052
charlieperson <charlie.person@hyperisland.se>
2016-08-13 21:11:51 -0500

    Adds exercise 38- not yet completed

---
+++ ex38.js
@@ -0,0 +1,8 @@
+function (clicks, saveData, name) {
+	return clicks.
+		// TODO: Throttle the clicks so that it only happens every one second
+		concatMap(function () {
+			return saveData(name);
+		});
+}
+		

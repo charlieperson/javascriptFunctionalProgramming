7bca30ecd0b561072591f4f99211ff314b3d8413
charlieperson <charlie.person@hyperisland.se>
2016-08-14 14:47:43 -0500

    Completes exercise 38

--- ex38.js
+++ ex38.js
@@ -1,8 +1,7 @@
 function (clicks, saveData, name) {
 	return clicks.
-		// TODO: Throttle the clicks so that it only happens every one second
+		throttle(1000).
 		concatMap(function () {
 			return saveData(name);
 		});
 }
-		

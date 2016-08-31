ce29551ddbc558c306036d66f41da29df4f87b0a
charlieperson <charlie.person@hyperisland.se>
2016-08-31 00:21:43 -0500

    Completes exercise 40

--- ex40.js
+++ ex40.js
@@ -2,14 +2,10 @@ function (keyPresses, isAlpha) {
 
 	return keyPresses.
 		map(function (e) { return String.fromCharCode(e.keyCode); }).
-
-		// Ensure we only have alphabetic characters
 		filter(function (character) { return isAlpha(character); }).
-
-		// TODO: Filter out successive repetitive keys
-
-		// Building up a string of all the characters typed.
+		distinctUntilChanged().
 		scan('', function (stringSoFar, character) {
 			return stringSoFar + character;
 		});
 }
+    
